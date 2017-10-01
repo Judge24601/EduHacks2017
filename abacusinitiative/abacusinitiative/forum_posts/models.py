6e18e00e3b885b14e7e7575283f510866d0b46f6
Judge24601 <braedenjury@gmail.com>
2017-10-01 00:29:11 -0700

    Forum Post Model

--- abacusinitiative/abacusinitiative/forum_posts/models.py
+++ abacusinitiative/abacusinitiative/forum_posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from accounts.models import Student
+from accounts.models import Instructor
 # Create your models here.
+class Forum_Post(models.model):
+    query = models.TextField()
+    poster = models.ForeignKey(
+        'Student',
+        on_delete = models.CASCADE,
+    )
+    posts = models.Manager()
+
+class Forum_Response(models.model):
+    text = models.TextField();
+    poster = models.ForeignKey(
+        'Student',
+        on_delete = models.CASCADE,
+    )
+    response_to = models.ForeignKey(
+        'Forum_Post',
+        on_delete = models.CASCADE,
+    )
+
+class Instructor_Response(models.model):
+    text = models.TextField();
+    poster = models.ForeignKey(
+        'Instructor',
+        on_delete = models.CASCADE,
+    )
+    response_to = models.ForeignKey(
+        'Forum_Post',
+        on_delete = models.CASCADE,
+    )
