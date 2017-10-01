150543e1a9db4e65a47db0a221b3845b8846ebab
Judge24601 <braedenjury@gmail.com>
2017-10-01 01:17:31 -0700

    Server in Running State

--- abacusinitiative/abacusinitiative/assignments/models.py
+++ abacusinitiative/abacusinitiative/assignments/models.py
@@ -1,50 +1,50 @@
 from django.db import models
-from accounts.models import Student
-from accounts.models import Instructor
+#from accounts.models import Student
+#from accounts.models import Instructor
 # Create your models here.
 class Question (models.Model):
     text = models.TextField()
     correct_ans_text = models.CharField(
         max_length = 255,
     )
-    correct_ans_num = models.DecimalField()
+    correct_ans_num = models.FloatField()
     ans_text = models.CharField(
         max_length = 255,
     )
-    ans_num = models.DecimalField()
+    ans_num = models.FloatField()
     ans_type = models.CharField(
         max_length = 20,
     )
 
-class Forum_Post(models.model):
+class Forum_Post(models.Model):
     query = models.TextField()
-    poster = models.ForeignKey(
-        'Student',
-        on_delete = models.CASCADE,
-    )
+    #poster = models.ForeignKey(
+    #    'Student',
+    #    on_delete = models.CASCADE,
+    #)
     linked_q = models.ForeignKey(
         'Question',
         on_delete = models.CASCADE,
     )
     posts = models.Manager()
 
-class Forum_Response(models.model):
+class Forum_Response(models.Model):
     text = models.TextField();
-    poster = models.ForeignKey(
-        'Student',
-        on_delete = models.CASCADE,
-    )
+    #poster = models.ForeignKey(
+    #    'Student',
+    #    on_delete = models.CASCADE,
+    #)
     response_to = models.ForeignKey(
         'Forum_Post',
         on_delete = models.CASCADE,
     )
 
-class Instructor_Response(models.model):
+class Instructor_Response(models.Model):
     text = models.TextField();
-    poster = models.ForeignKey(
-        'Instructor',
-        on_delete = models.CASCADE,
-    )
+    #poster = models.ForeignKey(
+    #    'Instructor',
+    #    on_delete = models.CASCADE,
+    #)
     response_to = models.ForeignKey(
         'Forum_Post',
         on_delete = models.CASCADE,
