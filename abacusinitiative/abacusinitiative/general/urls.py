3f9ad468335622249aeb571f5267ae45eb88096a
Judge24601 <braedenjury@gmail.com>
2017-10-01 03:28:21 -0700

    testing

--- abacusinitiative/abacusinitiative/general/urls.py
+++ abacusinitiative/abacusinitiative/general/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from assignments.views import questionView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    #temporary
+    url(r'^assignments', questionView.get(), name='question'),
 ]
